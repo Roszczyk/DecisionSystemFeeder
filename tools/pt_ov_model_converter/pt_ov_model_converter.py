f5126122ff0f5f30a772a8895ae72f937479d3cd
Roszczyk <mikolaj.roszczyk@onet.pl>
2025-08-01 23:40:40 +0200

    pt->ov converter works for YOLO

--- tools/pt_ov_model_converter/pt_ov_model_converter.py
+++ tools/pt_ov_model_converter/pt_ov_model_converter.py
@@ -1,7 +1,29 @@
 import torch
 import openvino as ov
 from pathlib import Path
+import argparse
 
-base_dir = Path(__file__).parent
-torch_model = base_dir / "model.pt"
-openvino_model = base_dir / "model_ov"
+def handle_args():
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument("--model", type=Path, help="path to pytorch model")
+    parser.add_argument("--arch", type=str, help="model architecture, currently supported: YOLO", choices=["YOLO"], default="YOLO")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = handle_args()
+    base_dir = Path(__file__).parent
+
+    if args.model == None:
+        torch_model = base_dir / "model.pt"
+        if not torch_model.exists():
+            print("ERROR: No model provided")
+            exit(1)
+    else:
+        torch_model = args.model
+        
+    architecture = args.arch
+    
+    if architecture == "YOLO":
+        from ultralytics import YOLO
+        model = YOLO(torch_model)
+        model.export(format="openvino")
