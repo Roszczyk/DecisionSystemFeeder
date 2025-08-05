d3240036fd96c511885b43d7adefe40613bb37d1
Roszczyk <mikolaj.roszczyk@onet.pl>
2025-08-06 01:10:34 +0200

    Project Gruchberta - begin, readme, recorder

---
+++ layer_1/CV_RGBCamera/Project_Gruchberta/README.md
@@ -0,0 +1,5 @@
+# Project Gruchberta
+
+A pigeon laid eggs on my balcony. This gave me an idea to create a testbed before the Feeder. There will be RGB camera, YOLO and the whole systems test conducted to keep an eye on the pigeons and document the incubation, hatching and the first days of baby pigeons. 
+
+I named the pigeon ```Gruchberta```, which is also the name of the whole side-project. 
---
+++ layer_1/CV_RGBCamera/Project_Gruchberta/recorder.py
@@ -0,0 +1,74 @@
+import cv2
+import os
+import time
+from datetime import datetime, timedelta
+from pathlib import Path
+import json
+
+RECORD_DIR = Path(__file__).parent / "recordings"
+CONFIG_FILE = Path(__file__).parent / "../config.json"
+RECORD_SECONDS = 60 * 60  #1 hour
+DELETE_OLDER_THAN_HOURS = 24
+FRAME_WIDTH = 640
+FRAME_HEIGHT = 480
+FPS = 20.0
+
+os.makedirs(RECORD_DIR, exist_ok=True)
+
+def get_camera_config(config_file):
+    with open(Path(__file__).parent / "config.json") as f:
+        config = json.load(f)
+    return config
+
+
+def delete_old_files():
+    now = datetime.now()
+    for filename in os.listdir(RECORD_DIR):
+        filepath = RECORD_DIR / filename
+        if not os.path.isfile(filepath):
+            continue
+        try:
+            file_time = datetime.strptime(filename, "%Y-%m-%d_%H.avi")
+            if now - file_time > timedelta(hours=DELETE_OLDER_THAN_HOURS):
+                os.remove(filepath)
+                print(f"Deleted {filename}")
+        except ValueError:
+            continue
+
+
+def main():
+    cam_no = get_camera_config(CONFIG_FILE)["webcam_no"]
+    cap = cv2.VideoCapture(cam_no)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
+
+    if not cap.isOpened():
+        print("Unable to run camera")
+        return
+
+    while True:
+        start_time = time.time()
+        now = datetime.now()
+        filename = now.strftime("%Y-%m-%d_%H.avi")
+        filepath = os.path.join(RECORD_DIR, filename)
+
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter(filepath, fourcc, FPS, (FRAME_WIDTH, FRAME_HEIGHT))
+
+        print(f"{now} Recording started: {filename}")
+
+        while time.time() - start_time < RECORD_SECONDS:
+            ret, frame = cap.read()
+            if not ret:
+                print("Camera failed")
+                break
+            out.write(frame)
+
+        out.release()
+        delete_old_files()
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
