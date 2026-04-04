d67874b2dc9264f428e4c29fec84732b2d9f5406
Roszczyk <mikolaj.roszczyk@onet.pl>
2026-04-04 02:46:01 +0200

    Binary classifier wip, sensor rebuild

--- layer_2/sensor.py
+++ layer_2/sensor.py
@@ -1,17 +1,8 @@
 from datetime import datetime
 
-class TruthTable:
-    def __init__(self, true_positive, true_negative, false_positive, false_negative):
-        self.true_positive = true_positive
-        self.true_negative = true_negative
-        self.false_positive = false_positive
-        self.false_negative = false_negative
-
 class Sensor:
-    def __init__(self, name : str, truth_table : TruthTable, precision : float,
-                 measure_function : function):
+    def __init__(self, name : str, precision : float, measure_function : function):
         self.name = name
-        self.truth_table = truth_table
         self.precision = precision
         self.measure_function = measure_function
 
