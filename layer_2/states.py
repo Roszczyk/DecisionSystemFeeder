507b9a3b5cede70d611755e5b3d0419490bcdcb4
Roszczyk <mikolaj.roszczyk@onet.pl>
2026-07-21 22:04:48 +0200

    [Layer 2] States classes

--- layer_2/states.py
+++ layer_2/states.py
@@ -4,6 +4,7 @@ class State:            # static defined State of the Environment
         self.probability = probability  # a priori probability of the event P(x_i)
 
 class StateMeasured:    # object returned by the Sensor
-    def __init__(self, fused_states : list[State], measured_mass : float):
+    def __init__(self, fused_states : list[State], measured_mass : float, friendly_name : str = "unnamed"):
+        self.friendly_name = friendly_name
         self.states = fused_states
         self.mass = measured_mass   # in Bayes - measured probability, DST - mass m(X)
