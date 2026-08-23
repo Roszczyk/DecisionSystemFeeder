0ad48d0b228ff9722d07af17f6882a332a6f633a
Roszczyk <mikolaj.roszczyk@onet.pl>
2026-08-24 01:09:32 +0200

    computer vision sensors + fixes in simple sensor

--- layer_2/src/simulation_cases/case_feeder.py
+++ layer_2/src/simulation_cases/case_feeder.py
@@ -6,6 +6,7 @@ from src.fusion import FusionLibrary
 from src.simulation import Simulation
 
 import random
+from copy import deepcopy
 
 def main_feeder_simulation(fusion_methods : list[str] = None, iterations : int = 1000, return_all_logs : bool = False, verbose : bool = True):
     # validate the methods
@@ -41,7 +42,11 @@ def main_feeder_simulation(fusion_methods : list[str] = None, iterations : int =
     def movement_func(env : Environment, correctness_probability : float) -> StateMeasured:
         is_moving = env.measure_metric("movement")
         measured_moving = is_moving and random.random() <= correctness_probability
-        return simplest_sensor_states_measured_possible[0] if measured_moving else simplest_sensor_states_measured_possible[1]
+        is_moving_state = deepcopy(simplest_sensor_states_measured_possible[0])
+        is_not_moving_state = deepcopy(simplest_sensor_states_measured_possible[1])
+        is_moving_state.mass = correctness_probability if measured_moving else 1.0 - correctness_probability
+        is_not_moving_state.mass = correctness_probability if not measured_moving else 1.0 - correctness_probability
+        return [is_moving_state, is_not_moving_state]
 
     movement_sensors = [
         SimpleSensor("01_movement_sensor_good", simplest_sensor_states_measured_possible, lambda e: movement_func(e, 0.95), 
@@ -81,8 +86,67 @@ def main_feeder_simulation(fusion_methods : list[str] = None, iterations : int =
     ]
     all_sensors += vibrations_sensors
 
-
-    # TODO Climate sensors (ScalarSensor)
     # TODO Camera sensors (ComputerVision)
+    cv_states_possible = [
+        StateMeasured([states[0]], friendly_name="bird"),
+        StateMeasured([states[1], states[2]], friendly_name="no bird")
+    ]
+    from src.sensor import BoundingBox
+    def get_bounding_boxes(environment : Environment, frequency_of_mistakes : float, detection_threshold : float = 0.7):
+        real_current_state = environment.get_current_state()
+        output_bb = []
+        if ("bird" in real_current_state.name and random.random() > frequency_of_mistakes) or \
+            ("bird" not in real_current_state.name and random.random() < frequency_of_mistakes):
+            if random.random() > 0.8:
+                number_of_birds = 2
+            else:
+                number_of_birds = 1
+        else:
+            number_of_birds = 0
+        # detect real birds
+        for i in range(number_of_birds):
+            x1 = random.random() / 2
+            x2 = x1 + (random.random() / 2)
+            y1 = random.random() / 2
+            y2 = y1 + (random.random() / 2)
+            confidence = random.random() * (1.0 - detection_threshold) + detection_threshold
+            output_bb.append(BoundingBox(x1, y1, x2, y2, confidence, "bird"))
+        # fake detections
+        number_of_fake = round(random.random()*3)
+        for i in range(number_of_fake):
+            x1 = random.random() / 2
+            x2 = x1 + (random.random() / 2)
+            y1 = random.random() / 2
+            y2 = y1 + (random.random() / 2)
+            confidence = random.random() * detection_threshold
+            output_bb.append(BoundingBox(x1, y1, x2, y2, confidence, "bird"))
+        return output_bb
+    def analyse_bounding_boxes(bb : list[BoundingBox]):
+        detected_birds = []
+        for box in bb:
+            if box.cls == "bird" and box.confidence >= 0.7:
+                detected_birds.append(box)
+        bird_detected_state = deepcopy(cv_states_possible[0])
+        bird_not_detected_state = deepcopy(cv_states_possible[1])
+        confidence_values = [x.confidence for x in detected_birds]
+        confidence_values.sort()
+        mass = confidence_values[0] if len(confidence_values) > 0 else 0.0
+        if len(confidence_values) > 1:
+            for value in confidence_values[1:]:
+                mass += (1.0 - mass) * value
+        bird_detected_state.mass = mass
+        bird_not_detected_state.mass = 1.0 - mass
+        return [bird_detected_state, bird_not_detected_state]
+
+    cv_sensors = [
+        ComputerVision("06_cv_rgb", lambda env: get_bounding_boxes(env, 0.05), 
+                       analyse_bounding_boxes, cv_states_possible),
+        ComputerVision("07_cv_ir", lambda env: get_bounding_boxes(env, 0.3, 0.6), 
+                       analyse_bounding_boxes, cv_states_possible)
+    ]
+    all_sensors += cv_sensors
+
+
+
 
     # TODO Run simulation
