9aeabd99463c03eaeabb257a130dd3aa684d77e9
Roszczyk <mikolaj.roszczyk@onet.pl>
2026-09-16 22:34:54 +0200

    better test

--- layer_1/test_ir_detection.py
+++ layer_1/test_ir_detection.py
@@ -89,37 +89,32 @@ print(ir_results)
 if 'Bird' in ir_results.label_names:
     ir_detected = True
 
-if (ir_detected or rgb_detected) and (now - last_photo_time) > COOLDOWN:
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-    img_path = f"{SAVE_DIR}/bird_{timestamp}.jpg"
-    txt_path = f"{SAVE_DIR}/birds_{timestamp}.txt"
-    bb_img_path = f"{SAVE_DIR}/bird_{timestamp}_bb.jpg"
-    img_rgb2_path = f"{SAVE_DIR}/bird_{timestamp}_RGB2.jpg"
-    ir_path = f"{SAVE_DIR}/bird_{timestamp}_ir.jpg"
-    bb_ir_path = f"{SAVE_DIR}/bird_{timestamp}_irbb.jpg"
+img_path = f"{SAVE_DIR}/bird_{timestamp}.jpg"
+txt_path = f"{SAVE_DIR}/birds_{timestamp}.txt"
+bb_img_path = f"{SAVE_DIR}/bird_{timestamp}_bb.jpg"
+img_rgb2_path = f"{SAVE_DIR}/bird_{timestamp}_RGB2.jpg"
+ir_path = f"{SAVE_DIR}/bird_{timestamp}_ir.jpg"
+bb_ir_path = f"{SAVE_DIR}/bird_{timestamp}_irbb.jpg"
 
-    cv2.imwrite(img_path, frame)
-    cv2.imwrite(ir_path, frame_ir)  
-    if ir_detected:
-        bb_ir_frame = visualise_result(frame_ir, ir_results)
-        cv2.imwrite(bb_ir_path, bb_ir_frame)
-    if rgb_detected:
-        cv2.imwrite(bb_img_path, frame_copy)
+cv2.imwrite(img_path, frame)
+cv2.imwrite(ir_path, frame_ir)  
+bb_ir_frame = visualise_result(frame_ir, ir_results)
+cv2.imwrite(bb_ir_path, bb_ir_frame)
+cv2.imwrite(bb_img_path, frame_copy)
 
-    # second RGB camera save:
-    if CAMERA_RGB_2 != -1:
-        rgb2_photo = take_frame(CAMERA_RGB_2)
-        if rgb2_photo is not None:
-            cv2.imwrite(img_rgb2_path, rgb2_photo)
+# second RGB camera save:
+if CAMERA_RGB_2 != -1:
+    rgb2_photo = take_frame(CAMERA_RGB_2)
+    if rgb2_photo is not None:
+        cv2.imwrite(img_rgb2_path, rgb2_photo)
 
-    with open(txt_path, "w") as f:
-        for name, conf, x1, y1, x2, y2 in bird_boxes:
-            f.write(f"{name} {conf:.3f} {x1} {y1} {x2} {y2}\n")
+with open(txt_path, "w") as f:
+    for name, conf, x1, y1, x2, y2 in bird_boxes:
+        f.write(f"{name} {conf:.3f} {x1} {y1} {x2} {y2}\n")
 
-    print(f"📸 Saved: {img_path}")
-    print(f"📦 Boxes: {txt_path}")
-    print(f"📸 Saved IR: {ir_path}")
-    print(f"📸 Saved with boxes: {bb_img_path}")
-
-    last_photo_time = now
+print(f"📸 Saved: {img_path}")
+print(f"📦 Boxes: {txt_path}")
+print(f"📸 Saved IR: {ir_path}")
+print(f"📸 Saved with boxes: {bb_img_path}")
